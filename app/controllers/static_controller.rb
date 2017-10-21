0c8131963134a202ab132436a21bf00db0553522
KaseyPasqualini <kaseypasqualini@yahoo.com>
2017-10-21 18:17:32 -0400

    test push 3

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,6 +4,21 @@ class StaticController < ApplicationController
       @terms = Term.all
 	end
 
+	def show_ruby
+			@var = Term.where(:language_id => 1)
+	end
+
+	def show_html
+		@var = Term.where(:language_id => 2)
+	end
+
+	def show_css
+		@var = Term.where(:language_id => 3)
+	end
+
+	def show_all_cards
+		@var = Term.all
+	end
 
 
 end
